4bf177e2736738aae944d214678a89f072acd1dd
arjunprakask <arjunprakashk7@gmail.com>
2025-11-27 14:58:08 +0530

    Fix owner approval issue with temporary user_type update route

--- PawsNest_Project/settings.py
+++ PawsNest_Project/settings.py
@@ -208,6 +208,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "arjunz.project@gmail.com"
-EMAIL_HOST_PASSWORD = "luzd vmyd anfd irvj"  # Use App Password, not normal password
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
